d4b8c5c241494252e09cb21a9ac92eeacb7ef63f
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-04 21:05:22 +0700

    practies 9

--- js/main.js
+++ js/main.js
@@ -4,9 +4,12 @@ Vue.component('product', {
         premium: {
             type: Boolean,
             required: true
-        }
-    },
-    template: `
+        },
+        cart: {
+            type: Array,
+            required: true
+        },
+        template: `
         <div class="product">
          <div class="product-image">
             <img :src="image" :alt="altText"/>
@@ -33,86 +36,88 @@ Vue.component('product', {
             <ul>
                 <li v-for="size in sizes">{{size}}</li>
             </ul>
-            <div class="cart">
-                <p>Cart {{ cart }}</p>
-            </div>
             <button v-on:click="addToCart"
             :disabled="!inStock"
             :class="{ disabledButton: !inStock}">Add to cart</button>
             <div class="cart-redu">
         </div>
         <button v-on:click="reduToCart">Reduce to cart</button>
+        <button v-on:click="removeFromCart">Remove from cart</button>
             <p v-if="inStock">In stock</p>
             <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
         </div>
         </div>
 `,
-    data() {
-        return {
-            product: "Socks",
-            brand: 'Vue Mastery',
-            description: "A pair of warm, fuzzy socks",
-            selectedVariant: 0,
-            altText: "A pair of socks",
-            link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-            inventory: 100,
-            onSale: true,
-            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-            variants: [
-                {
-                    variantId: 2234,
-                    variantColor: 'green',
-                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                    variantQuantity: 10
-                },
-                {
-                    variantId: 2235,
-                    variantColor: 'blue',
-                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 0
+        data() {
+            return {
+                product: "Socks",
+                brand: 'Vue Mastery',
+                description: "A pair of warm, fuzzy socks",
+                selectedVariant: 0,
+                altText: "A pair of socks",
+                link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+                inventory: 100,
+                onSale: true,
+                details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+                variants: [
+                    {
+                        variantId: 2234,
+                        variantColor: 'green',
+                        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                        variantQuantity: 10
+                    },
+                    {
+                        variantId: 2235,
+                        variantColor: 'blue',
+                        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                        variantQuantity: 0
+                    }
+                ],
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            }
+        },
+        methods: {
+            addToCart() {
+                this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+            },
+            removeFromCart() {
+                this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
+            },
+            updateProduct(index) {
+                this.selectedVariant = index;
+                console.log(index);
+            },
+            reduToCart() {
+                this.cart -= 1
+            }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product;
+            },
+            image() {
+                return this.variants[this.selectedVariant].variantImage;
+            },
+            inStock() {
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            sale() {
+                if (this.onSale) {
+                    return this.brand + ' ' + this.product + ' ' + 'is on sale!';
+                } else {
+                    return this.brand + ' ' + this.product + ' ' + 'is not on sale!';
                 }
-            ],
-            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            },
+            shipping() {
+                if (this.premium) {
+                    return "Free";
+                } else {
+                    return 2.99
+                }
+            },
         }
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-            console.log(index);
-        },
-        reduToCart() {
-            this.cart -= 1
-        }
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale(){
-            if(this.onSale){
-                return this.brand + ' ' + this.product + ' ' + 'is on sale!';
-            }else{
-                return this.brand + ' ' + this.product + ' ' + 'is not on sale!';
-            }
-        },
-        shipping() {
-            if (this.premium) {
-                return "Free";
-            } else {
-                return 2.99
-            }
-        },
     }
-} )
+})
 
 Vue.component('product-details', {
     props: {
@@ -131,6 +136,17 @@ let app = new Vue ({
     el: '#app',
     data: {
         premium: true,
-        cart: 0,
+        cart: [],
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id);
+            if (index > -1) {
+                this.cart.splice(index, 1);
+            }
+        }
     }
 })
